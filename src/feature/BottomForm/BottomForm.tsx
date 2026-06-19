7f3bfca16536e6cfdc5d58d9b69acf2b2a890f06
denimubarokhilmi <hilmimubarokdeni@gmail.com>
2026-06-19 22:20:19 +0700

    fix:replace and change some elements

--- src/feature/BottomForm/BottomForm.tsx
+++ src/feature/BottomForm/BottomForm.tsx
@@ -6,6 +6,7 @@ import { fetchCocktailThunk } from "../../shared/redux/action";
 import LoadingSpinner from "../../shared/components/LoadingSpinner";
 import IngredientField from "../../shared/components/IngredientField";
 import MeasureField from "../../shared/components/MeasureField";
+import { Box } from "@mui/material";
 
 function BottomForm() {
   const dispatch = useDispatch<any>();
@@ -19,47 +20,70 @@ function BottomForm() {
   }, []);
 
   const handleClicks = useCallback(() => {
-    if (selected.trim().length == 0) return setErrorField("input is required");
+    if (!selected) return setErrorField("input is required");
     dispatch(fetchCocktailThunk(selected));
     setErrorField("");
     return;
   }, [dispatch, selected]);
 
   return (
-    <section className="bottom-form">
-      <div className="mt-5 content-form-top bg-white p-4 position-relative 100 z-1 d-flex flex-column justify-content-between flex-wrap rounded-5">
-        <h6>CockTail : </h6>
+    <section className="bottom-form p-2">
+      <div className="container mt-5 bg-white p-4 position-relative z-1 d-flex flex-column rounded-5 shadow-sm">
+        <Box
+          sx={{
+            width: {
+              xs: "100%",
+              sm: "500px",
+              md: "600px",
+            },
+          }}
+        >
+          <div className="row justify-content-between align-items-center ">
+            <div className="col-sm-5 mb-2">
+              <h6 className="form-label fw-bold mb-2">CockTail :</h6>
+              <SearchInput onChange={onChange}></SearchInput>
+              {errorField && (
+                <p className="text-danger small mt-1 mb-0">{errorField}</p>
+              )}
+            </div>
 
-        <div className="d-flex flex-wrap gap-4">
-          <div className="flex-lg-shrink-1 flex-grow-1">
-            <SearchInput onChange={onChange}></SearchInput>
-            {errorField && <p className=" text-danger">{errorField}</p>}
+            <div className="col-sm-5">
+              <br />
+              <SearchButton
+                onClicks={handleClicks}
+                disabled={false}
+              ></SearchButton>
+            </div>
           </div>
+        </Box>
 
-          <div className="flex-lg-shrink-1 flex-grow-1">
-            <SearchButton
-              onClicks={handleClicks}
-              disabled={false}
-            ></SearchButton>
-          </div>
-        </div>
-
+        {/* loading spinner */}
         {data?.loading && (
-          <LoadingSpinner label="seacrh cocktail"></LoadingSpinner>
+          <div>
+            <LoadingSpinner label="searching cocktail..."></LoadingSpinner>
+          </div>
         )}
 
-        {/* error handling  */}
+        {/* error handling */}
         {data?.error && (
-          <>
-            <h6 className=" text-center mt-3 text-danger">{data?.error}</h6>
-          </>
+          <h6 className="text-center mt-4 text-danger fw-bold">
+            {data?.error}
+          </h6>
         )}
 
-        {data?.ingredient?.strIngredient1 && (
-          <>
-            <div className="row">
-              <div className="col-6 col-md-4 col-sm-6 col-xxl">
-                <h6 className="mt-3">Ingredient : </h6>
+        {data?.ingredient && (
+          <Box
+            sx={{
+              width: {
+                xs: "100%",
+                sm: "500px",
+                md: "600px",
+              },
+            }}
+          >
+            <div className="row mt-4 pt-3 g-3">
+              <div className="col-6">
+                <h6 className="fw-bold  mb-3">Ingredient :</h6>
                 {Object.entries(data?.ingredient).map(([key, val]) => (
                   <IngredientField
                     key={key}
@@ -69,8 +93,8 @@ function BottomForm() {
                 ))}
               </div>
 
-              <div className="col-6 col-md-4 col-sm-6 col-xxl">
-                <h6 className="mt-3">Qty : </h6>
+              <div className="col-6">
+                <h6 className="fw-bold  mb-3">Qty :</h6>
                 {Object.entries(data?.measure).map(([key, val]) => (
                   <MeasureField
                     key={key}
@@ -80,6 +104,11 @@ function BottomForm() {
                 ))}
               </div>
             </div>
+          </Box>
+        )}
+        {!data?.ingredient && (
+          <>
+            <h6 className=" text-center mt-2">please look for a cocktail!</h6>
           </>
         )}
       </div>
--- src/feature/TopForm/TopForm.tsx
+++ src/feature/TopForm/TopForm.tsx
@@ -1,29 +1,30 @@
 import { useSelector } from "react-redux";
 import IngredientList from "../../shared/components/IngredientList";
 import MeasureList from "../../shared/components/MeasureList";
-
 function TopForm() {
   const data = useSelector((state: any) => state.cocktail);
 
   return (
     <section className="top-form bg-white position-relative z-2">
-      <div className="content-form-top bg-white p-3 position-relative 100 z-1 d-flex flex-wrap justify-content-between">
-        {data?.ingredient?.strIngredient1 ? (
+      <div className="content-form-top bg-white p-3 position-relative  z-1 d-flex flex-wrap justify-content-between align-items-center gap-3">
+        {data?.ingredient ? (
           <>
-            <div className="left-box flex-grow-1 flex-shrink-1  pe-4">
-              <IngredientList ingredient={data?.ingredient}></IngredientList>
-            </div>
-            <div className="right-box flex-grow-1 flex-shrink-1 ">
-              <MeasureList measure={data?.measure}></MeasureList>
+            <div className="row align-items-center">
+              <div className="col-sm-5">
+                <IngredientList ingredient={data?.ingredient}></IngredientList>
+              </div>
+              <div className="col-sm-5">
+                <MeasureList measure={data?.measure}></MeasureList>
+              </div>
             </div>
           </>
         ) : (
           <div className=" flex-grow-1 flex-shrink-1 text-center ps-5 pe-5">
-            <h6>no ingredient selected </h6>
+            <h6>no ingredient selected</h6>
           </div>
         )}
       </div>
-      {data?.ingredient?.strIngredient1 && (
+      {data?.ingredient && (
         <>
           <div className="total-ingredient text-end pe-4">
             <h5>Total Ingredient : {Object.keys(data?.ingredient).length} </h5>
